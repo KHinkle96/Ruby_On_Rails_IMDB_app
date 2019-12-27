535c1456c32c5d19fb2a201a24400e552fd80047
Katie Hinkle <katie@Katies-MacBook-Air.local>
2019-12-27 15:18:21 -0800

    added bundle and created games model

---
+++ db/migrate/20191227231000_create_games.rb
@@ -0,0 +1,10 @@
+class CreateGames < ActiveRecord::Migration[6.0]
+  def change
+    create_table :games do |t|
+      t.string :title
+      t.date :release_date
+      t.string :rating
+      t.string :console
+    end
+  end
+end
