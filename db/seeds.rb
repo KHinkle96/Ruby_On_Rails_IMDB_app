f1741d52b9cb406b13ef952c9068bbf1189e4a4b
Katie Hinkle <katie@Katies-MacBook-Air.local>
2020-01-02 17:06:44 -0800

    added faker gem, added games controller, made the home page display

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do |number|
+    title = Faker::Game.title
+    release_date = Faker::Game.date
+    rating = Faker::Game.rating
+    console = Faker::Game.console
