163a2e5ebaa6ccb33b82021efccb37b7e070e768
Katie Hinkle <katie@Katies-MacBook-Air.local>
2020-01-04 18:39:42 -0800

    added search bar and navigation buttons, You can enter data but it will not update

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,8 @@
+class SearchesController < ApplicationController
+end
+
+def index
+    @query = params[:q]
+    @games = Game.where("body ILIKE ?", "%#{@query}%").order(created_at: :desc)
+  end
+end
